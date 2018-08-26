f12a5858d33f4753459e22c3fbf33ec2909f7dee
Mohanned Abuzant <mohannedabuzant@gmail.com>
2018-08-26 15:05:59 +0300

    Create NewsLoader.java

---
+++ app/src/main/java/com/channel/example/newsapp1/NewsLoader.java
@@ -0,0 +1,39 @@
+package com.channel.example.newsapp1;
+
+
+import android.content.Context;
+import android.content.AsyncTaskLoader;
+
+import java.util.List;
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+    private static final String LOG_TAG = NewsLoader.class.getName();
+
+    /**
+     * Query URL
+     */
+    private String mUrl;
+
+    public NewsLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * This is on a background thread.
+     */
+    @Override
+    public List<News> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<News> earthquakes = QueryUtils.fetchNewsData(mUrl);
+        return earthquakes;
+    }
+}
